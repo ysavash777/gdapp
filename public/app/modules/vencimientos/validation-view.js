60f663da6fd1042296a5d994b6e07065368fac91
ysavash777 <yvngsavash@gmail.com>
2026-08-19 21:31:40 -0300

    Vencimientos: el selector de ubicación deja de tener contenedor propio
    Elimina el wrapper .venc-loc-bar; #locNav pasa a ser un .venc-acc-item
    más, primero dentro de .venc-acc-body, con el mismo margen/fondo/radio
    que Caja/Producto/Observación en vez de reproducir el fondo a mano.

--- public/app/modules/vencimientos/validation-view.js
+++ public/app/modules/vencimientos/validation-view.js
@@ -94,14 +94,12 @@ export function openValidation(initialItem, { onDone, pendingItems = [] }) {
   const overlay = document.createElement('div');
   overlay.className = 'scan-overlay';
   overlay.innerHTML = `
-    <div class="venc-loc-bar">
-      <div class="venc-loc-nav" id="locNav">
+    <div class="scan-sheet cq-sheet venc-acc-body" id="vencAccBody">
+      <div class="venc-acc-item venc-loc-nav" id="locNav">
         <button type="button" class="venc-loc-arrow" id="locPrev" title="Posición anterior">${icon('chevronLeft', 20)}</button>
         <span class="venc-loc-label">Seleccionar ubicación</span>
         <button type="button" class="venc-loc-arrow" id="locNext" title="Posición siguiente">${icon('chevronRight', 20)}</button>
       </div>
-    </div>
-    <div class="scan-sheet cq-sheet venc-acc-body" id="vencAccBody">
       <div class="venc-acc-item" data-state="pending" id="itemCaja">
         <button type="button" class="venc-acc-head" id="headCaja">
           <span class="venc-acc-avatar" id="avatarCaja">${icon('clock', 18)}</span>
